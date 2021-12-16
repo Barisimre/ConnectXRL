647a29e3280bbfb1916dc3385da85e68698b0ee5
Baris Imre <barisimre98@gmail.com>
2021-12-16 16:57:25 +0100

    Add RL

--- rl_player.py
+++ rl_player.py
@@ -6,8 +6,8 @@ class Player():
 
     def __init__(self) -> None:
         self.model = Model()
-        self.buffer = []
-        self.TRESH = 100
+        self.training = True
+
 
 
     def make_move(self, observation):
@@ -17,18 +17,8 @@ class Player():
 
 
     def save(self, old_obs, action, reward, new_obs):
-        self.buffer.append([old_obs, action, reward, new_obs])
+        if self.training:
+            self.model.save(old_obs, action, reward, new_obs)
         
-        if len(self.buffer) > self.TRESH:
-            self.train()
-            self.buffer = []
-
-
-    def train(self):
-        # Sample
-        # Pass to network
-        # Update
-        pass
-
 
     
