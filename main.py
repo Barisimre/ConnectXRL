6e483524e60c2a987779763d62038682cccd7917
Jarik <jarikgerbertkarsten@gmail.com>
2021-12-16 16:13:39 +0100

    test

--- main.py
+++ main.py
@@ -1,26 +1,2 @@
-from kaggle_environments import make, agent
-
-# System declaration
-configuration = {
-    "rows": 5,
-    "columns":5,
-    "inarow": 3,
-    "actTimeout": 10
-}
-
-def step(env, custom_agent):
-    trainer = env.train([None, "negamax"])
-
-    obs = trainer.reset()
-    for _ in range(1000):
-        agent1_action = custom_agent.act(env.state[0].observation)
-        obs, reward, done, info = trainer.step(agent1_action[0])
-        print(reward)
-        if done:
-            obs = trainer.reset()
-
-env = make("connectx", configuration=configuration)
-custom_agent = agent.Agent("random", env)
-step(env, custom_agent)
 
 
--- test.py
+++ test.py
@@ -1,23 +1,32 @@
 from kaggle_environments import make
 from rl_player import *
 
-env = make("connectx", debug=True)
+configuration = {
+    "rows": 6,
+    "columns:": 7,
+    "inarow": 4,
+    "actTimeout": 10
+}
 
-# Training agent in first position (player 1) against the default random agent.
-trainer = env.train([None, "random"])
-done = False
-obs = trainer.reset()
+env = make("connectx", configuration=configuration, debug=True)
 
-player = Player()
+def train():
+    # Training agent in first position (player 1) against the default random agent.
+    trainer = env.train([None, "random"])
+    obs = trainer.reset()
 
-for _ in range(1000):
-    # env.render()
-    
-    if env.done:
-        obs = trainer.reset()
-    old_obs = obs
-    action = player.make_move(obs)
-    obs, reward, done, info = trainer.step(action)
-    player.save(old_obs, action, reward, obs)
-    print(reward, done)
+    player = Player()
+
+    for _ in range(1000):
+        # env.render()
+
+        if env.done:
+            obs = trainer.reset()
+        old_obs = obs
+        action = player.make_move(obs)
+        obs, reward, done, info = trainer.step(action)
+        player.save(old_obs, action, reward, obs)
+        print(reward, done)
+
+train()
     
